990af4918120bf2937009944d30691c2d46df3df
Season <seasonluke@gmail.com>
2024-01-07 12:27:10 +0800

    feat: image loading

--- test_cuda_encode.py
+++ test_cuda_encode.py
@@ -22,7 +22,7 @@ for path in image_paths:
 
 height = np.array(height).astype(np.int32)
 width = np.array(width).astype(np.int32)
-ic(images[0][0][0], images[1][0][0])
+ic(images[0][0:3][0], images[1][0:3][0])
 # in practise there might be varying batch-sizes
 # to enable batching in cuda_encode, we need to flatten all images,
 # concat them into single array with their heights info
